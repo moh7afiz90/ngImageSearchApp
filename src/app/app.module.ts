6a797dbf297e596a760ecc0f554f2add8d1dadf6
Mohanad Elhag <moh.7afiz@gmail.com>
2017-07-04 02:44:10 +0300

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms'
-
+// components
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './components/image-list/image-list.component';
-
+// Services
 import { ImageService } from './shared/image.service';
+// CSS frameworks
+import { MaterialModule } from '@angular/material';
+import { MasonryModule } from 'angular2-masonry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +21,10 @@ import { ImageService } from './shared/image.service';
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    MaterialModule,
+    MasonryModule,
+    BrowserAnimationsModule
   ],
   providers: [ImageService],
   bootstrap: [AppComponent]
